b0b3dd0dfce4dd63aaa3a5e0c4214188f90fca3c
ksigvaldason <114832663+ksigvaldason@users.noreply.github.com>
2024-12-14 21:24:29 +0000

    74%

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,12 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by!(username: params[:id])
+    @pending_requests = @user.received_follow_requests.where(status: "pending") if @user.private?
   end
-  
+
+  def feed
+    @user = User.find_by!(username: params[:id])
+    following_ids = @user.sent_follow_requests.where(status: "accepted").pluck(:recipient_id)
+    @feed_photos = Photo.where(owner_id: following_ids).order(created_at: :desc)
+  end
 end
